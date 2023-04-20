24dce8ed14113d8735e0d461588c4d514395c786
caseylam3 <125606876+caseylam3@users.noreply.github.com>
2023-04-20 03:25:32 -0400

    Update airtable.py

--- airtable.py
+++ airtable.py
@@ -2,46 +2,177 @@ import time
 import requests
 import json 
 
-URL = ''
+URL = 'https://api.airtable.com/v0/appxFSx1vodDWljqe/TaskOrder?api_key=keyKf7fEzdNw4S7qi'
 
 
 response = requests.get(url = URL, params = {})
-data = response.json()
+datatask = response.json()
 
 def getReady():
     # retrieves value for transport task for picking up the cup at the start
-# retrieves value for transport task for picking up the cup at the start
-    cup_pick_up = dataTask['records'][1]['fields']['Ready for Robot']
+    Rcup = dataTask['records'][1]['fields']['Ready for Robot']
+ 
+    Rice = dataTask['records'][2]['fields']['Ready for Robot']
+    
+    Rsmall = dataTask['records'][3]['fields']['Ready for Robot']
 
-    # retrieves value for transport task for going to portioning
-    ice_pick_up = dataTask['records'][2]['fields']['Deployed?']
+    Rprocessed = dataTask['records'][4]['fields']['Ready for Robot']
 
-    # retrieves value for portioning task to begin portioning
-    portioning = dataTask['records'][3]['fields']['Deployed?']
+    Rblender1 = dataTask['records'][5]['fields']['Ready for Robot']
+
+    Rblender2 = dataTask['records'][6]['fields']['Ready for Robot']
+
+    Rpickup1 = dataTask['records'][7]['fields']['Ready for Robot']
+
+    Rpickup2 = dataTask['records'][8]['fields']['Ready for Robot']
+
+    
+    return Rcup, Rice, Rsmall, Rprocessed, Rblender1, Rblender2, Rpickup1, Rpickup2
+
+def getDocked():
+    # retrieves value for transport task for picking up the cup at the start
+    # retrieves value for transport task for picking up the cup at the start
+    Dcup = dataTask['records'][1]['fields']['Robot Docked']
+ 
+    # retrieves value for ice station open status
+    Dice = dataTask['records'][2]['fields']['Robot Docked']
+
+    # retrieves value for small foods and liquids station open status
+    Dsmall = dataTask['records'][3]['fields']['Robot Docked']
 
     # retrieves value for transport task to pick up portioned items
-    transport3 = dataTask['records'][4]['fields']['Deployed?']
+    Dprocessed = dataTask['records'][4]['fields']['Robot Docked']
 
     # retrieves value for transport task to go to processing
-    transport4 = dataTask['records'][5]['fields']['Deployed?']
+    Dblender1 = dataTask['records'][5]['fields']['Robot Docked']
 
     # value for processing task to process food
-    processing = dataTask['records'][6]['fields']['Deployed?']
+    Dblender2 = dataTask['records'][6]['fields']['Robot Docked']
 
     # value for transport task to pick up processed 
-    transport5 = dataTask['records'][7]['fields']['Deployed?']
+    Dpickup1 = dataTask['records'][7]['fields']['Robot Docked']
 
     # value for transport to bring to blending
-    transport6 = dataTask['records'][8]['fields']['Deployed?']
+    Dpickup2 = dataTask['records'][8]['fields']['Robot Docked']
 
-    # value for blending to start blending process
-    blending = dataTask['records'][9]['fields']['Deployed?']
+    
+    return Dcup, Dice, Dsmall, Dprocessed, Dblender1, Dblender2, Dpickup1, Dpickup2
 
-    # value for blending to clean - can happen asynchronously from chronological process
-    blendingC = dataTask['records'][10]['fields']['Deployed?']
+def getFinish():
+    # retrieves value for transport task for picking up the cup at the start
+    # retrieves value for transport task for picking up the cup at the start
+    Fcup = dataTask['records'][1]['fields']['Tasked Finished']
+ 
+    # retrieves value for ice station open status
+    Fice = dataTask['records'][2]['fields']['Tasked Finished']
 
-    transport7 = dataTask['records'][11]['fields']['Deployed?']
+    # retrieves value for small foods and liquids station open status
+    Fsmall = dataTask['records'][3]['fields']['Tasked Finished']
 
-    return transport1, transport2, transport3, transport4, transport5, transport6, transport7, portioning, processing, blending, blendingC
+    # retrieves value for transport task to pick up portioned items
+    Fprocessed = dataTask['records'][4]['fields']['Tasked Finished']
 
+    # retrieves value for transport task to go to processing
+    Fblender1 = dataTask['records'][5]['fields']['Tasked Finished']
+
+    # value for processing task to process food
+    Fblender2 = dataTask['records'][6]['fields']['Tasked Finished']
+
+    # value for transport task to pick up processed 
+    Fpickup1 = dataTask['records'][7]['fields']['Tasked Finished']
+
+    # value for transport to bring to blending
+    Fpickup2 = dataTask['records'][8]['fields']['Tasked Finished']
+
+    
+    return Fcup, Fice, Fsmall, Fprocessed, Fblender1, Fblender2, Fpickup1, Fpickup2
   
+def main(args=None):
+    startProcess = True
+    #initialize process by setting all fields to ready for robot/ not sure if needed
+    dataTask['records'][1]['fields']['Ready for Robot'] = 1
+    dataTask['records'][2]['fields']['Ready for Robot'] = 1
+    dataTask['records'][3]['fields']['Ready for Robot'] = 1
+    dataTask['records'][4]['fields']['Ready for Robot'] = 1
+    dataTask['records'][5]['fields']['Ready for Robot'] = 1
+    dataTask['records'][6]['fields']['Ready for Robot'] = 1
+    dataTask['records'][7]['fields']['Ready for Robot'] = 1
+    dataTask['records'][8]['fields']['Ready for Robot'] = 1
+    
+    while startProcess:
+        #retrieve updated values of each field from other robots
+        Rcup, Rice, Rsmall, Rprocessed, Rblender1, Rblender2, Rpickup1, Rpickup2 = getReady()
+        
+        Dcup, Dice, Dsmall, Dprocessed, Dblender1, Dblender2, Dpickup1, Dpickup2 = getDocked()
+        
+        Fcup, Fice, Fsmall, Fprocessed, Fblender1, Fblender2, Fpickup1, Fpickup2 = getFinish()
+        
+        #conditions for getting cup:
+        if Rcup == 1:
+            Fcup = Dcup = 0 #not sure if i should use this variable or use whole json definition
+        if Dcup == 1:
+            Rcup = Fcup = 0
+        if Fcup == 1:
+            Dcup = Rcup = 0
+        
+        #conditions for getting ice:
+        if Rice == 1:
+            Fice = Dice = 0 
+        if Dcup == 1:
+            Rice = Fice = 0
+        if Fcup == 1:
+            Dice = Rice = 0
+            
+        #conditions for getting small foods and liquids:
+        if Rsmall == 1:
+            Fsmall = Dsmall = 0 
+        if Dcup == 1:
+            Rsmall = Fsmall = 0
+        if Fcup == 1:
+            Dsmall = Rsmall = 0
+            
+        #conditions for getting processed foods:
+        if Rprocessed == 1:
+            Fprocessed = Dprocessed = 0 
+        if Dprocessed == 1:
+            Rprocessed = Fprocessed = 0
+        if Fprocessed == 1:
+            Dprocessed = Rprocessed = 0
+            
+        #conditions for getting blended in blender 1:
+        if Rblender1 == 1:
+            Fblender1 = Dblender1 = 0 
+        if Dblender1 == 1:
+            Rblender1 = Fblender1 = 0
+        if Fblender1 == 1:
+            Dblender1 = Rblender1 = 0
+            
+            
+        #conditions for getting blended in blender 2:
+        if Rblender2 == 1:
+            Fblender2 = Dblender2 = 0 
+        if Dblender2 == 1:
+            Rblender2 = Fblender2 = 0
+        if Fblender2 == 1:
+            Dblender1 = Rblender1 = 0
+            
+        #conditions for getting picked up at 1:
+        if Rpickup1 == 1:
+            Fpickup1 = Dpickup1 = 0 
+        if Dpickup1 == 1:
+            Rpickup1 = Fpickup1 = 0
+        if Fpickup1 == 1:
+            Dpickup1 = Rpickup1 = 0
+            
+        #conditions for getting picked up at 2:
+        if Rpickup2 == 1:
+            Fpickup2 = Dpickup2 = 0 
+        if Dpickup2 == 1:
+            Rpickup2 = Fpickup2 = 0
+        if Fpickup2 == 1:
+            Dpickup2 = Rpickup2 = 0
+            
+            
+      
+    
+        

24aca97cd39c748e8c666e66353d5c840ace2752
caseylam3 <125606876+caseylam3@users.noreply.github.com>
2023-04-20 03:57:35 -0400

    Update airtable.py

--- airtable.py
+++ airtable.py
@@ -9,7 +9,7 @@ response = requests.get(url = URL, params = {})
 datatask = response.json()
 
 def getReady():
-    # retrieves value for transport task for picking up the cup at the start
+    # retrieves boolean for if station is ready or not
     Rcup = dataTask['records'][1]['fields']['Ready for Robot']
  
     Rice = dataTask['records'][2]['fields']['Ready for Robot']
@@ -30,66 +30,96 @@ def getReady():
     return Rcup, Rice, Rsmall, Rprocessed, Rblender1, Rblender2, Rpickup1, Rpickup2
 
 def getDocked():
-    # retrieves value for transport task for picking up the cup at the start
-    # retrieves value for transport task for picking up the cup at the start
+    # retrieves boolean for if robot is docked or not
     Dcup = dataTask['records'][1]['fields']['Robot Docked']
  
-    # retrieves value for ice station open status
     Dice = dataTask['records'][2]['fields']['Robot Docked']
 
-    # retrieves value for small foods and liquids station open status
     Dsmall = dataTask['records'][3]['fields']['Robot Docked']
 
-    # retrieves value for transport task to pick up portioned items
     Dprocessed = dataTask['records'][4]['fields']['Robot Docked']
 
-    # retrieves value for transport task to go to processing
     Dblender1 = dataTask['records'][5]['fields']['Robot Docked']
 
-    # value for processing task to process food
     Dblender2 = dataTask['records'][6]['fields']['Robot Docked']
 
-    # value for transport task to pick up processed 
     Dpickup1 = dataTask['records'][7]['fields']['Robot Docked']
 
-    # value for transport to bring to blending
     Dpickup2 = dataTask['records'][8]['fields']['Robot Docked']
-
     
     return Dcup, Dice, Dsmall, Dprocessed, Dblender1, Dblender2, Dpickup1, Dpickup2
 
 def getFinish():
-    # retrieves value for transport task for picking up the cup at the start
-    # retrieves value for transport task for picking up the cup at the start
+    # retrieves boolean for if robot is finished with task
     Fcup = dataTask['records'][1]['fields']['Tasked Finished']
  
-    # retrieves value for ice station open status
     Fice = dataTask['records'][2]['fields']['Tasked Finished']
 
-    # retrieves value for small foods and liquids station open status
     Fsmall = dataTask['records'][3]['fields']['Tasked Finished']
 
-    # retrieves value for transport task to pick up portioned items
     Fprocessed = dataTask['records'][4]['fields']['Tasked Finished']
 
-    # retrieves value for transport task to go to processing
     Fblender1 = dataTask['records'][5]['fields']['Tasked Finished']
 
-    # value for processing task to process food
     Fblender2 = dataTask['records'][6]['fields']['Tasked Finished']
 
-    # value for transport task to pick up processed 
     Fpickup1 = dataTask['records'][7]['fields']['Tasked Finished']
 
-    # value for transport to bring to blending
     Fpickup2 = dataTask['records'][8]['fields']['Tasked Finished']
-
     
     return Fcup, Fice, Fsmall, Fprocessed, Fblender1, Fblender2, Fpickup1, Fpickup2
+
+def getCoordinates(): #retrieves coordiates for every task
+    Cupx = datatask['records'][1]['fields']['X Coordinate']
+    Cupy = datatask['records'][1]['fields']['Y Coordinate']  
+    Cupz = datatask['records'][1]['fields']['Z Coordinate'] 
+    Cupw = datatask['records'][1]['fields']['W Coordinate'] 
+    
+    Icex = datatask['records'][2]['fields']['X Coordinate']
+    Icey = datatask['records'][2]['fields']['Y Coordinate']  
+    Icez = datatask['records'][2]['fields']['Z Coordinate'] 
+    Icew = datatask['records'][2]['fields']['W Coordinate']
+    
+    Smallx = datatask['records'][3]['fields']['X Coordinate']
+    Smally = datatask['records'][3]['fields']['Y Coordinate']  
+    Smallz = datatask['records'][3]['fields']['Z Coordinate'] 
+    Smallw = datatask['records'][3]['fields']['W Coordinate']
+    
+    Prox = datatask['records'][4]['fields']['X Coordinate']
+    Proy = datatask['records'][4]['fields']['Y Coordinate']  
+    Proz = datatask['records'][4]['fields']['Z Coordinate'] 
+    Prow = datatask['records'][4]['fields']['W Coordinate']
+    
+    B1x = datatask['records'][5]['fields']['X Coordinate']
+    B1y = datatask['records'][5]['fields']['Y Coordinate']  
+    B1z = datatask['records'][5]['fields']['Z Coordinate'] 
+    B1w = datatask['records'][5]['fields']['W Coordinate']
+    
+    B2x = datatask['records'][6]['fields']['X Coordinate']
+    B2y = datatask['records'][6]['fields']['Y Coordinate']  
+    B2z = datatask['records'][6]['fields']['Z Coordinate'] 
+    B2w = datatask['records'][6]['fields']['W Coordinate']
+    
+    P1x = datatask['records'][7]['fields']['X Coordinate']
+    P1y = datatask['records'][7]['fields']['Y Coordinate']  
+    P1z = datatask['records'][7]['fields']['Z Coordinate'] 
+    P1w = datatask['records'][7]['fields']['W Coordinate']
+    
+    P2x = datatask['records'][7]['fields']['X Coordinate']
+    P2y = datatask['records'][7]['fields']['Y Coordinate']  
+    P2z = datatask['records'][7]['fields']['Z Coordinate'] 
+    P2w = datatask['records'][7]['fields']['W Coordinate']
   
+    return Cupx, Cupy, Cupz, Cupw, Icex, Icey, Icez, Icew, Smallx, Smally, Smallz, Smallw, Prox, Proy, Proz, Prow, B1x, B1y, B1z, B1w, B2x, B2y, B2z, B2w, P1x, P1y, P1z, P1w, P2x, P2y, P2z, P2w
+
+
 def main(args=None):
     startProcess = True
-    #initialize process by setting all fields to ready for robot/ not sure if needed
+    
+    rclpy.init(args=args)
+    action_client = MovementActionClient()
+
+    #initialize process by setting all fields to ready for robot
     dataTask['records'][1]['fields']['Ready for Robot'] = 1
     dataTask['records'][2]['fields']['Ready for Robot'] = 1
     dataTask['records'][3]['fields']['Ready for Robot'] = 1
@@ -100,12 +130,14 @@ def main(args=None):
     dataTask['records'][8]['fields']['Ready for Robot'] = 1
     
     while startProcess:
-        #retrieve updated values of each field from other robots
+        #retrieve updated values of each field 
         Rcup, Rice, Rsmall, Rprocessed, Rblender1, Rblender2, Rpickup1, Rpickup2 = getReady()
         
         Dcup, Dice, Dsmall, Dprocessed, Dblender1, Dblender2, Dpickup1, Dpickup2 = getDocked()
         
         Fcup, Fice, Fsmall, Fprocessed, Fblender1, Fblender2, Fpickup1, Fpickup2 = getFinish()
+        
+        Cupx, Cupy, Cupz, Cupw, Icex, Icey, Icez, Icew, Smallx, Smally, Smallz, Smallw, Prox, Proy, Proz, Prow, B1x, B1y, B1z, B1w, B2x, B2y, B2z, B2w, P1x, P1y, P1z, P1w, P2x, P2y, P2z, P2w = getCoordinates()
         
         #conditions for getting cup:
         if Rcup == 1:
@@ -118,10 +150,13 @@ def main(args=None):
         #conditions for getting ice:
         if Rice == 1:
             Fice = Dice = 0 
+            action_client.send_navigate(Icex, Icey, Icez, Icew) #when ice station is ready, robot will move to that station
         if Dcup == 1:
             Rice = Fice = 0
+            self.send_dock() # once robot gets to coordinate, robot will dock
         if Fcup == 1:
             Dice = Rice = 0
+            self.send_undock() #once task is finished, robot will undock
             
         #conditions for getting small foods and liquids:
         if Rsmall == 1:
@@ -145,8 +180,7 @@ def main(args=None):
         if Dblender1 == 1:
             Rblender1 = Fblender1 = 0
         if Fblender1 == 1:
-            Dblender1 = Rblender1 = 0
-            
+            Dblender1 = Rblender1 = 0     
             
         #conditions for getting blended in blender 2:
         if Rblender2 == 1:
@@ -170,9 +204,15 @@ def main(args=None):
         if Dpickup2 == 1:
             Rpickup2 = Fpickup2 = 0
         if Fpickup2 == 1:
-            Dpickup2 = Rpickup2 = 0
+            Dpickup2 = Rpickup2 = 0            
             
-            
-      
+        try:
+            rclpy.spin(action_client)
+        
+        except KeyboardInterrupt:
+            rclpy.shutdown()
+        
+if __name__ == '__main__':
+    main()
     
         
